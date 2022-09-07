5ddacea2ec4355a875351b305332e08d04fc285c
jzhou45 <jonathanzhou77@gmail.com>
2022-09-07 06:18:22 -0400

    add css to home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,10 +4,58 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import concat from "../images/concat.png";
 import metabook from "../images/metabook.png";
 import olympus from "../images/olympus.png";
+import "../stylesheets/home.css";
+import $ from "jquery";
 
 const Home = () => {
+    let words =["Hi there,", "I'm Jonathan", "Welcome to my page!"],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 120;
+
+    const wordflick = () => {
+        setInterval(() => {
+            if (forwards) {
+                if (offset >= words[i].length){
+                    ++skip_count;
+                    if (skip_count === skip_delay){
+                        forwards = false;
+                        skip_count = 0;
+                    };
+                };
+            } else{
+                if (offset === 0){
+                    forwards = true;
+                    i++;
+                    offset = 0;
+                    if (i >= len){
+                        i = 0;
+                    };
+                };
+            };
+            part = words[i].substring(0, offset);
+            if (skip_count === 0){
+                if (forwards){
+                    offset++
+                } else{
+                    offset--
+                };
+            };
+            $('.word').text(part);
+        }, speed);
+    };
+
+    $(function () {
+        wordflick();
+    });
+
     return(
-        <div className="Home">
+        <div className="home">
             <Carousel
                 infiniteLoop 
                 autoPlay	
@@ -16,6 +64,7 @@ const Home = () => {
                 showThumbs={false} 
                 showStatus={false}
                 showArrows={false}
+                className="home-carousel"
             >
                 <div>
                     <img src={concat} alt=".concat" />
@@ -29,6 +78,8 @@ const Home = () => {
                     <img src={olympus} alt="olympus card-jitsu" />
                 </div>
             </Carousel>
+
+            <div className="word"></div>
         </div>
     );
 };
