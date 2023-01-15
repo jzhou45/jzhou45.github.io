218248b431ce728a4eef21ebe7ae5f7899284ce5
jzhou45 <jonathanzhou77@gmail.com>
2023-01-14 19:27:39 -0500

    update home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,45 +5,97 @@ import concat from "../images/concat.png";
 import metabook from "../images/metabook.png";
 import olympus from "../images/olympus.png";
 import "../stylesheets/home.css";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+    const [state, setState] = useState({
+        funcText: "",
+        consoleText: "",
+        consoleText2: "",
+        closeText: "",
+    });
+
+    const {funcText, consoleText, consoleText2, closeText} = state;
+
+    useEffect(() => {
+        let funcN = 0;
+        let funcStr = "function helloThere( ){";
+        let consoleN = 0;
+        let consoleStr = "console.log('I am Jonathan Zhou \\n);";
+        let consoleN2 = 0;
+        let consoleStr2 = "console.log('and Welcome to my Page');";
+        let closeN = 0;
+        let closeStr = "};"
+        let blink = 0;
+        let timer = setInterval(() => {
+            if (funcN !== funcStr.length){
+                funcN++;
+                setState({
+                    ...state,
+                    funcText: funcStr.slice(0, funcN)
+                });
+            } else if (consoleN !== consoleStr.length - 1){
+                consoleN++;
+                setState({
+                    ...state,
+                    funcText: funcStr,
+                    consoleText: consoleStr.slice(0, consoleN)
+                });
+            } else if (consoleN2 !== consoleStr2.length){
+                consoleN2++;
+                setState({
+                    ...state,
+                    funcText: funcStr,
+                    consoleText: consoleStr,
+                    consoleText2: consoleStr2.slice(0, consoleN2)
+                });
+            } else{
+                closeN++;
+                setState({
+                    ...state,
+                    funcText: funcStr,
+                    consoleText: consoleStr,
+                    consoleText2: consoleStr2,
+                    closeText: closeStr.slice(0, closeN)
+                });
+                if (closeN === closeStr.length){
+                    clearInterval(timer);
+                    setInterval(() => {
+                        if (blink === 0){
+                            setState({
+                                ...state,
+                                funcText: funcStr,
+                                consoleText: consoleStr,
+                                consoleText2: consoleStr2,
+                                closeText: closeStr + "|"
+                            });
+                            blink = 1;
+                        } else{
+                            setState({
+                                ...state,
+                                funcText: funcStr,
+                                consoleText: consoleStr,
+                                consoleText2: consoleStr2,
+                                closeText: closeStr
+                            });
+                            blink = 0;
+                        };
+                    }, 500);
+                };
+            };
+        }, 60);
+    }, [])
+
+
     return(
         <div id="home">
-            <Carousel
-                infiniteLoop 
-                autoPlay	
-                interval={4000} 
-                stopOnHover={false} 
-                showThumbs={false} 
-                showStatus={false}
-                showArrows={false}
-                swipeable={false}
-                className="home-carousel"
-            >
-                <div>
-                    <img src={concat} alt=".concat" />
-                </div>
-
-                <div>
-                    <img src={metabook} alt="metabook" />
-                </div>
-
-                <div>
-                    <img src={olympus} alt="olympus card-jitsu" />
-                </div>
-            </Carousel>
-
-            <h1 className="word">Jonathan Zhou</h1>
-
-            <div id="gotohome">Nothing to see here</div>
-
-            <div className="swipe-down-div">
-                <img 
-                    src="https://cdn-icons-png.flaticon.com/512/25/25623.png" 
-                    alt="swipe down" 
-                    className="swipe-down"
-                />
+            <div>
+                <p className="func">{funcText}</p>
+                <p className="console">{consoleText}</p>
+                <p className="console2">{consoleText2}</p>
+                <p className="close">{closeText}</p>
             </div>
+            <div id="gotohome"></div>
         </div>
     );
 };
